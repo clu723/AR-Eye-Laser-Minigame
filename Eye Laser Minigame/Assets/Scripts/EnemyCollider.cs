7cc471ebdaa6571ae2e03eb41bbc115bdd5a12f5
Chang Lu <clu723@berkeley.edu>
2023-03-23 15:16:57 -0700

    collider update

---
+++ Eye Laser Minigame/Assets/Scripts/EnemyCollider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Laser"))
+        {
+            Destroy(this.gameObject);
+        }
+            
+    }
+}
